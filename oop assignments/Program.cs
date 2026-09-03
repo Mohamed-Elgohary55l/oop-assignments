df32255545eb7d52be70b870593bba155500fac1
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-09-04 02:15:28 +0300

    q1 a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified? b) What happens when a Customer variable is copied into another variable and one variable modifies the object

--- oop assignments/Program.cs
+++ oop assignments/Program.cs
@@ -4,7 +4,145 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region OOP1 assignment
+            /***********************/
+
+            #region q1 a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+            // if the new copy is modified the original variable will not be affected because they are two different variables and they are a value type.
+            #endregion
+            /***********************/
+            #region b) What happens when a Customer variable is copied into another variable and one variable modifies the object
+
+            // if the new copy is modified the original variable will be affected because they are two different variables but they are a reference type.
+            #endregion
+
+            /***********************/
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #region
+
+            #endregion
+            /***********************/
+
+            #endregion
         }
     }
 }
